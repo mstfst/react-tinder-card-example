42121bf1f2d5a6f048be35b63cb7e17ad8a818ac
DESKTOP-U235624\fractal <gamesthatcan@gmail.com>
2021-03-20 21:34:37 -0400

    adding google tag manager

--- src/cardData.js
+++ src/cardData.js
@@ -10,22 +10,37 @@ export const cards = {
     
     What do you do?`,
     leftText: 'Wait outside',
-    leftCardID: 'police_arrive',
+    leftCardID: 'police_arrive_wait',
     rightText: 'Go in',
-    rightCardID: 'police_arrive'
+    rightCardID: 'police_arrive_inside'
   },
-  police_arrive: {
-    text: `The police arrive.`,
+  police_arrive_wait: {
+    text: `The police arrive.
+    
+    The police believe that you were acting as a "lookout" for your friend.  They arrest you for break and enter.
+    
+    The police can let you go until your criminal charges get resolved, but they take you to jail to await your first bail hearing instead.`,
+    leftText: 'Go to jail',
+    leftCardID: 'in_jail',
+    rightText: 'Go to jail',
+    rightCardID: 'in_jail'
+  },
+  in_jail: {
+    text: `You are in jail.
+    
+    You will wait there until your bail hearing, which will happen within the next 24 hours.
+    
+    In the hearing, a Judicial Officer will determine your bail.`,
     leftText: 'Wait',
-    leftCardID: 'end',
-    rightText: 'Run',
-    rightCardID: 'end'
+    leftCardID: 'bail_hearing',
+    rightText: 'Wait',
+    rightCardID: 'bail_hearing'
   },
-  end: {
-    text: `The last card`,
-    leftText: 'end',
-    leftCardID: null,
-    rightText: 'end',
-    rightCardID: null
-  }
+ bail_hearing: {
+  text: 'You are at the bail hearing',
+  leftText: 'end',
+  leftCardID: null,
+  rightText: 'end',
+  rightCardID: null
+ }
 };
